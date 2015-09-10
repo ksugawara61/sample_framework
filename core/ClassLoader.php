4e97d23f65bf6de67a8566b664835f73537b1c47
ksugawara61 <katsuo.com61@gmail.com>
2015-09-10 23:45:38 +0900

    ClassLoader.phpの追加：オートロードに関する処理をまとめたクラス

---
+++ core/ClassLoader.php
@@ -0,0 +1,32 @@
+<?php
+
+// オートロードに関する処理をまとめたクラス
+class ClassLoader
+{
+  protected $dirs;
+
+  // PHPにオートローダクラスを登録する処理
+  public function register()
+  {
+    spl_autoload_register(array($this, 'localhost'));
+  }
+
+  // ディレクトリを登録する処理
+  public function registerDir($dir)
+  {
+    $this->dirs[] = $dir;
+  }
+
+  // クラスファイルの読み込みを行う処理
+  public function loadClass($class)
+  {
+    foreach ($this->dirs as $dir) {
+      $file = $dir . '/' . $class . '.php';
+      if (is_readable($file)) {
+        require $file;
+
+        return;
+      }
+    }
+  }
+}
