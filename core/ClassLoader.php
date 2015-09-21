2fff216571a77989b803629c6e7f200384229a92
ksugawara61 <katsuo.com61@gmail.com>
2015-09-21 21:56:03 +0900

    ClassLoader.phpの修正：インデントをタブに修正

--- core/ClassLoader.php
+++ core/ClassLoader.php
@@ -3,30 +3,30 @@
 // オートロードに関する処理をまとめたクラス
 class ClassLoader
 {
-  protected $dirs;
+	protected $dirs;
 
-  // PHPにオートローダクラスを登録する処理
-  public function register()
-  {
-    spl_autoload_register(array($this, 'localhost'));
-  }
+	// PHPにオートローダクラスを登録する処理
+	public function register()
+	{
+		spl_autoload_register(array($this, 'localhost'));
+	}
 
-  // ディレクトリを登録する処理
-  public function registerDir($dir)
-  {
-    $this->dirs[] = $dir;
-  }
+	// ディレクトリを登録する処理
+	public function registerDir($dir)
+	{
+		$this->dirs[] = $dir;
+	}
 
-  // クラスファイルの読み込みを行う処理
-  public function loadClass($class)
-  {
-    foreach ($this->dirs as $dir) {
-      $file = $dir . '/' . $class . '.php';
-      if (is_readable($file)) {
-        require $file;
+	// クラスファイルの読み込みを行う処理
+	public function loadClass($class)
+	{
+		foreach ($this->dirs as $dir) {
+			$file = $dir . '/' . $class . '.php';
+			if (is_readable($file)) {
+				require $file;
 
-        return;
-      }
-    }
-  }
+				return;
+			}
+		}
+	}
 }
