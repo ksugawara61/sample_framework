bd77af0fe245d58dc2a5d32bac1fad15fd77be1d
ksugawara61 <katsuo.com61@gmail.com>
2015-10-25 13:04:59 +0900

    Controller.phpの更新：runメソッドの追加

--- core/Controller.php
+++ core/Controller.php
@@ -22,4 +22,21 @@ abstract class Controller
 		$this->session = $application->getSession();
 		$this->db_manager = $application->getDbManager();
 	}
+
+	// Applicationクラスから呼び出され、実際にアクションを実行するメソッド
+	public function run($action, $params = array())
+	{
+		$this->action_name = $action;
+
+		// アクションとなるメソッドの名前を格納
+		$action_method = $action . 'Action';
+		if (!method_exists($this, $action_method)) {
+			$this->forward404();
+		}
+
+		// アクションの実行
+		$content = $this->action_method($params);
+
+		return $content;
+	}
 }
